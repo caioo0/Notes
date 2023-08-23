a6cef5d8cc443d073b1c1d08ddb4c90c96a723b3
caioo0 <cai_oo@sina.com>
2023-08-23 17:45:00 +0800

    .

--- note-coding/docs/test.py
+++ note-coding/docs/test.py
@@ -2,7 +2,7 @@ import os
 def getfiles(path1):
     for filepath,dirnames,filenames in os.walk(path1):
             for filename in filenames:
-                print ('* [%s](./docs/rust/rust_Bible/%s)' %(filename,filename))
+                print ('* [%s](./docs/go/%s)' %(filename,filename))
 
-path1 = r"D:\www\learning\caioo0.github.io\note-coding\docs\rust\rust_Bible"
+path1 = r"D:\www\learning\caioo0.github.io\note-coding\docs\go"
 getfiles(path1)
